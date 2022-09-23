c40d61948256100091b3ae837e7d211c4220fbe4
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-09-23 20:24:03 +0300

    Completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,7 +2,19 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+  const projectArray = projects.map(project => {
+    return <ProjectItem 
+  
+    key={project.id}
+        name = {project.name}
+        about = {project.about}
+       technologies= {project.technologies}
+      
+    />
+  })
+
   console.log(projects);
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
